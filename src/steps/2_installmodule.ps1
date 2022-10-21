35ad606ae78409c7712c8515ef2f72c959ad5d42
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-20 21:36:21 -0400

    Switch to JFrog for downloading prerelease module

--- src/steps/2_installmodule.ps1
+++ src/steps/2_installmodule.ps1
@@ -1,5 +1,5 @@
 if (!(Get-InstalledModule -Name ChangelogManagement -ErrorAction SilentlyContinue)) {
     Write-Host "Installing ChangelogManagement PowerShell Module..."
-    Register-PSRepository -Name PoshTestGallery -SourceLocation "https://www.poshtestgallery.com/api/v2/" -InstallationPolicy Trusted
-    Install-Module -Name ChangelogManagement -Repository PoshTestGallery -AllowPrerelease
+    Register-PSRepository -Name JFrog -SourceLocation "https://natescherer.jfrog.io/artifactory/api/nuget/nuget" -InstallationPolicy Trusted
+    Install-Module -Name ChangelogManagement -Repository JFrog -AllowPrerelease
 }
