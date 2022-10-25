edb6f4d516e1454020e85dbc926c91754ffee7e9
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-25 00:40:01 -0400

    Send releaseNotes as multiline

--- src/steps/5_readdata.ps1
+++ src/steps/5_readdata.ps1
@@ -5,4 +5,6 @@ $ResolvedPath = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_PATH
 $Data = Get-ChangelogData -Path $ResolvedPath
 
 "lastVersion=$($Data.LastVersion)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
-"releaseNotes=$($Data.ReleaseNotes)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+"releaseNotes<<EOF" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+$Data.ReleaseNotes | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+"EOF" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
