e1dee98b7e827bb944a170b7daaabb6324db1188
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-25 00:27:49 -0400

    Remove escaping on outputs

--- src/steps/5_readdata.ps1
+++ src/steps/5_readdata.ps1
@@ -1,13 +1,8 @@
 Write-Host "Reading changelog data..."
 
-$ResolvedPath = "$env:GITHUB_WORKSPACE\$env:INPUT_PATH"
+$ResolvedPath = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_PATH
 
 $Data = Get-ChangelogData -Path $ResolvedPath
 
-. "$env:GITHUB_ACTION_PATH\src\functions\ConvertTo-ActionsEscapedString.ps1"
-
-$SafeVersion = ConvertTo-ActionsEscapedString -String $Data.LastVersion
-$SafeNotes = ConvertTo-ActionsEscapedString -String $Data.ReleaseNotes
-
-"lastVersion=$SafeVersion" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
-"releaseNotes=$SafeNotes" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+"lastVersion=$($Data.LastVersion)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
+"releaseNotes=$($Data.ReleaseNotes)" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf-8 -Append
