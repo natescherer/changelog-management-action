4e0d5ac57a5ec07050eddfba23878fbed896167d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2022-10-25 00:27:57 -0400

    Switch to Join-Path

--- src/steps/3_addchange.ps1
+++ src/steps/3_addchange.ps1
@@ -1,5 +1,5 @@
 Write-Host "Adding Change..."
 
-$ResolvedPath = "$env:GITHUB_WORKSPACE\$env:INPUT_PATH"
+$ResolvedPath = Join-Path $env:GITHUB_WORKSPACE $env:INPUT_PATH
 
 Add-ChangelogData -Path $ResolvedPath -Type $env:INPUT_CHANGETYPE -Value $env:INPUT_CHANGEVALUE
